61f2d309ca40b83a23c35f4b912027bd6548887b
Daniel Ntege <danientege785@gmail.com>
2025-05-31 19:06:59 +0300

    commit

--- src/auth/app.js
+++ src/auth/app.js
@@ -128,7 +128,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      maxAge: 24 * 60 * 60 * 1000,
     },
   }),
 )
